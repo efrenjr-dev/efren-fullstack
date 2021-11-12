abc1ae3ae2dc732a701b4491092d3e6537d9244b
efrenjr-dev <efrenjr.rodiguez@gmail.com>
2021-11-13 02:08:12 +0300

    email form validation

--- email.js
+++ email.js
@@ -8,16 +8,28 @@ function sendEmail() {
   let subject = document.getElementById("category-select").value;
   let messageBody = document.getElementById("message-input").value;
 
+  console.log(senderEmail);
+  console.log(senderName);
+  console.log(messageBody);
+
+  if (
+    senderEmail !== "" &&
+    senderName !== "" &&
+    messageBody !== ""
+  ) {
+    Email.send({
+      SecureToken: "fc250b0d-038a-4119-87a7-7f6c169bd222 ",
+      To: "efrenjr.rodriguez@gmail.com",
+      From: "efrenjr.rodriguez@gmail.com",
+      Subject: `[${subject}] ${senderName}`,
+      Body: `<html><h3>From: ${senderName}</h3><h5>Reply-to: ${senderEmail}</h5><p>${messageBody}</p></html>`,
+    }).then((message) => alert(message));
+  } else {
+    alert("Email Form: All fields are required.");
+  }
+
   // console.log(senderEmail);
   // console.log(senderName);
   // console.log(subject);
   // console.log(messageBody);
-
-  Email.send({
-    SecureToken: "fc250b0d-038a-4119-87a7-7f6c169bd222 ",
-    To: "efrenjr.rodriguez@gmail.com",
-    From: "efrenjr.rodriguez@gmail.com",
-    Subject: `[${subject}] ${senderName}`,
-    Body: `<html><h3>From: ${senderName}</h3><h5>Reply-to: ${senderEmail}</h5><p>${messageBody}</p></html>`,
-  }).then((message) => alert(message));
 }
